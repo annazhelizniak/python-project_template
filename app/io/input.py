6c69213ac56f24abd3995e718f3b02ffa3ada571
Anna Zhelizniak <anna.zhelizniak08@gmail.com>
2024-03-15 14:37:37 +0200

    Add empty functions for processing user's input from console and file, and saving output to console and file

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_from_console():
+    pass
+
+
+def input_from_file(file):
+    pass
+
+
+def input_from_file_with_pandas(file):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_to_console():
+    pass
+
+
+def output_to_file(file):
+    pass
