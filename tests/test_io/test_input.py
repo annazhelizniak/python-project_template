a2f3e87909289ab25d942ed7a53aa4ec94c5a651
Anna Zhelizniak <anna.zhelizniak08@gmail.com>
2024-03-15 16:25:08 +0200

    Add tests

---
+++ tests/test_io/test_input.py
@@ -0,0 +1,31 @@
+from app.io.input import input_from_file, input_from_file_with_pandas
+import unittest
+import pandas as pd
+
+
+class TestInput(unittest.TestCase):
+    def test_input_from_file_exist(self):
+        text = input_from_file("../../data/text_sample")
+        self.assertIsInstance(text, str)
+        self.assertTrue(len(text) > 0)
+
+    def test_input_from_file_not_exist(self):
+        with self.assertRaises(FileNotFoundError):
+            input_from_file("data/not_existing_file.txt")
+
+    def test_input_from_file_builtin_empty(self):
+        file_content = input_from_file("../../data/empty")
+        self.assertEqual(file_content, "")
+
+    def test_input_from_file_pandas_exist(self):
+        df = input_from_file_with_pandas("../../data/csv_sample.csv")
+        self.assertIsNotNone(df)
+        self.assertEqual(df.shape, (4, 2))
+
+    def test_input_from_file_pandas_not_exist(self):
+        with self.assertRaises(FileNotFoundError):
+            input_from_file_with_pandas("data/not_existing_file.csv")
+
+    def test_input_from_file_pandas_wrong_format(self):
+        with self.assertRaises(pd.errors.ParserError):
+            input_from_file_with_pandas("../../data/text_sample")
